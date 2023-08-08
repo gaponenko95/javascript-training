20ae31b2e9c079d2e2940a389f533c38c4ac24a0
ag_gaponenko <sashagaponenko01904@yandex.ru>
2023-08-08 15:55:04 +1100

    step4-Основы

---
+++ step4-Основы/Arithmetic-operators.js
@@ -0,0 +1,21 @@
+// Базовые арифметические операторы
+
+const width = 10;
+const height = 5;
+const space = width * height;
+
+console.log(space)
+
+const newWidthSmall = width - 4;
+const newWidthBig = width + 4;
+const division = newWidthBig / newWidthSmall;
+const volume = 2 ** 3; // 2 * 2 * 2
+
+console.log(volume)
+
+// Строковые операторы
+
+const city = 'Moscow';
+const street = 'Novoslobodskaya';
+
+console.log(city + ', ' + street)
---
+++ step4-Основы/Assignment-and-comparison-operators.js
@@ -0,0 +1,22 @@
+// Операторы присваивания
+
+let age = 18;
+age += 2; // age = age + 2
+age -= 3; // age = age - 3
+age *= 3; // age = age * 3
+age /= 3; // age = age / 3
+
+age++; // age = age + 1
+age--; // age = age - 1
+
+console.log(age);
+
+// Операторы сравнения
+
+const vasyaAge = 20;
+console.log(age >  vasyaAge);
+console.log(age >= vasyaAge);
+console.log(age <  vasyaAge);
+console.log(age <= vasyaAge);
+console.log(age == vasyaAge);
+
