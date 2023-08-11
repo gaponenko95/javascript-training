9a0ece45cabb45e6882f9bdf381d1ad2187c9fee
Aleksander Gaponenko <sashagaponenko01904@yandex.ru>
2023-08-12 00:48:19 +1100

    Variables names
    Поменял названия переменных a и b на calcLong и calcLat

--- 4-Navigator/main.js
+++ 4-Navigator/main.js
@@ -3,9 +3,9 @@ const addressLong = 131;
 const positionLat = 46;
 const positionLong = 142;
 
-const a = positionLong - addressLong;
-const b = positionLat - addressLat;
+const calcLong = positionLong - addressLong;
+const calcLat = positionLat - addressLat;
 
-const distance = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+const distance = Math.sqrt(Math.pow(calcLong, 2) + Math.pow(calcLat, 2));
 
 console.log(`${distance} км`)
