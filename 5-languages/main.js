9980d42d69776b6c8e3b8d99e38fe1de89e0b717
Aleksander Gaponenko <sashagaponenko01904@yandex.ru>
2023-08-12 00:58:00 +1100

    Switch (langSelector)
    Заменил true на langSelector и смог избавиться дальше от него

--- 5-languages/main.js
+++ 5-languages/main.js
@@ -1,15 +1,15 @@
 const langSelector = prompt("Please, select a language: Ru, Eng, De!");
 
-switch (true) {
-  case langSelector === "Ru":
+switch (langSelector) {
+  case "Ru":
     console.log("Добро пожаловать!");
     break;
-  case langSelector === "Eng":
+  case "Eng":
     console.log("Welcome!");
     break;
-  case langSelector === "De":
+  case "De":
     console.log("Willkommen!");
     break;
   default:
     console.log("error");
-};
+}
