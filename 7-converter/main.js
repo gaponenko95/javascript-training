3419bf958350843072afae7cd229898f8e08e204
Aleksander Gaponenko <sashagaponenko01904@yandex.ru>
2023-11-22 14:07:54 +0300

    Update main.js

--- 7-converter/main.js
+++ 7-converter/main.js
@@ -1,64 +1,92 @@
 function RUB_USD_EUR(amount, curCurrency, getCurrency) {
-  curCurrency === "RUB";
-  if (getCurrency === "USD") {
-    return amount * 0.0099 + getCurrency;
-  } else if (getCurrency === "EUR") {
-    return amount * 0.009 + getCurrency;
-  } else if (getCurrency === "RUB") {
-    return amount + getCurrency;
-  } else {
-    return null;
-  }
-}
-function USD_RUB_EUR(amount, curCurrency, getCurrency) {
-  curCurrency === "USD";
-  if (getCurrency === "RUB") {
-    return amount * 100 + getCurrency;
-  } else if (getCurrency === "EUR") {
-    return amount * 0.91 + getCurrency;
-  } else if (getCurrency === "USD") {
-    return amount + getCurrency;
-  } else {
-    return null;
-  }
-}
-function EUR_USD_RUB(amount, curCurrency, getCurrency) {
-  curCurrency === "EUR";
-  if (getCurrency === "USD") {
-    return amount * 0.91 + getCurrency;
-  } else if (getCurrency === "RUB") {
-    return amount * 110.68 + getCurrency;
-  } else if (getCurrency === "EUR") {
-    return amount + getCurrency;
-  } else {
-    return null;
-  }
-}
-// Первая общая функция
-function currencyConverter(amount, curCurrency, getCurrency) {
-  if (curCurrency === "EUR") {
-    return EUR_USD_RUB(amount, curCurrency, getCurrency);
-  } else if (curCurrency === "USD") {
-    return USD_RUB_EUR(amount, curCurrency, getCurrency);
-  } else if (curCurrency === "RUB") {
-    return RUB_USD_EUR(amount, curCurrency, getCurrency);
-  } else {
-    return null;
-  }
-}
-// Вторая общая функция
-function currencyConverter2(amount, curCurrency, getCurrency) {
-  switch (curCurrency) {
-    case "EUR":
-      return EUR_USD_RUB(amount, curCurrency, getCurrency);
+  curCurrency === "RUB"
+  switch (getCurrency) {
     case "USD":
-      return USD_RUB_EUR(amount, curCurrency, getCurrency);
+      return amount * 87.77
+    case "EUR":
+      return amount * 96.19
     case "RUB":
-      return RUB_USD_EUR(amount, curCurrency, getCurrency);
+      return amount
     default:
-      return null;
+      return null
+  }
+  
+}
+
+function USD_RUB_EUR(amount, curCurrency, getCurrency) {
+  curCurrency === "USD"
+  switch (getCurrency) {
+    case "USD":
+      return amount
+    case "EUR":
+      return amount * 0.91
+    case "RUB":
+      return amount * 87.77
+    default:
+      return null
   }
 }
 
-console.log(currencyConverter(1000, "RUB", "USD"));
-console.log(currencyConverter2(1000, "RUB", "USD"));
+function EUR_USD_RUB(amount, curCurrency, getCurrency) {
+  curCurrency === "EUR"
+  switch (getCurrency) {
+    case "USD":
+      return amount * 1.10
+    case "EUR":
+      return amount
+    case "RUB":
+      return amount * 96.19
+    default:
+      return null
+  }
+}
+
+function currencyConverter(amount, curCurrency, getCurrency) {
+  switch (curCurrency) {
+    case "USD":
+      return USD_RUB_EUR(amount, curCurrency, getCurrency)
+    case "EUR":
+      return EUR_USD_RUB(amount, curCurrency, getCurrency)
+    case "RUB":
+      return RUB_USD_EUR(amount, curCurrency, getCurrency)
+    default:
+      return null
+  }
+}
+
+console.log(currencyConverter(1000, "RUB", "RUB"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
