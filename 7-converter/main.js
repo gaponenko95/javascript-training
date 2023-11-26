a7c3cb5b1aebf02807abd157e17eee960ffb38cd
Aleksander Gaponenko <sashagaponenko01904@yandex.ru>
2023-11-26 19:12:15 +0300

    Update main.js
    Внес правки, все поместилось в одну функцию

--- 7-converter/main.js
+++ 7-converter/main.js
@@ -1,92 +1,57 @@
-function RUB_USD_EUR(amount, curCurrency, getCurrency) {
-  curCurrency === "RUB"
-  switch (getCurrency) {
-    case "USD":
-      return amount * 87.77
-    case "EUR":
-      return amount * 96.19
-    case "RUB":
-      return amount
-    default:
-      return null
-  }
-  
-}
+function currencyConverter(amount, sourceCurrency, targetCurrency) {
+  let rate;
 
-function USD_RUB_EUR(amount, curCurrency, getCurrency) {
-  curCurrency === "USD"
-  switch (getCurrency) {
+  switch (sourceCurrency) {
     case "USD":
-      return amount
+      switch (targetCurrency) {
+        case "USD":
+          rate = 1;
+          break;
+        case "EUR":
+          rate = 0.91;
+          break;
+        case "RUB":
+          rate = 87.77;
+          break;
+        default:
+          rate = null;
+      }
+      break;
     case "EUR":
-      return amount * 0.91
+      switch (targetCurrency) {
+        case "USD":
+          rate = 1.1;
+          break;
+        case "EUR":
+          rate = 1;
+          break;
+        case "RUB":
+          rate = 96.19;
+          break;
+        default:
+          rate = null;
+      }
+      break;
     case "RUB":
-      return amount * 87.77
+      switch (targetCurrency) {
+        case "USD":
+          rate = 1 / 87.77;
+          break;
+        case "EUR":
+          rate = 1 / 96.19;
+          break;
+        case "RUB":
+          rate = 1;
+          break;
+        default:
+          rate = null;
+      }
+      break;
     default:
-      return null
+      rate = null;
   }
-}
 
-function EUR_USD_RUB(amount, curCurrency, getCurrency) {
-  curCurrency === "EUR"
-  switch (getCurrency) {
-    case "USD":
-      return amount * 1.10
-    case "EUR":
-      return amount
-    case "RUB":
-      return amount * 96.19
-    default:
-      return null
-  }
-}
-
-function currencyConverter(amount, curCurrency, getCurrency) {
-  switch (curCurrency) {
-    case "USD":
-      return USD_RUB_EUR(amount, curCurrency, getCurrency)
-    case "EUR":
-      return EUR_USD_RUB(amount, curCurrency, getCurrency)
-    case "RUB":
-      return RUB_USD_EUR(amount, curCurrency, getCurrency)
-    default:
-      return null
-  }
+  return rate !== null ? amount * rate : null;
 }
 
 console.log(currencyConverter(1000, "RUB", "RUB"));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
