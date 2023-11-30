b2b4c49735a989cb1ec0a08615cefcd9f29e224c
Aleksander Gaponenko <sashagaponenko01904@yandex.ru>
2023-11-30 11:36:03 +0300

    Update main.js
    Внес правки, очень интересный подход, спасибо!

--- 7-converter/main.js
+++ 7-converter/main.js
@@ -1,57 +1,30 @@
-function currencyConverter(amount, sourceCurrency, targetCurrency) {
-  let rate;
-
-  switch (sourceCurrency) {
-    case "USD":
-      switch (targetCurrency) {
-        case "USD":
-          rate = 1;
-          break;
-        case "EUR":
-          rate = 0.91;
-          break;
-        case "RUB":
-          rate = 87.77;
-          break;
-        default:
-          rate = null;
-      }
-      break;
-    case "EUR":
-      switch (targetCurrency) {
-        case "USD":
-          rate = 1.1;
-          break;
-        case "EUR":
-          rate = 1;
-          break;
-        case "RUB":
-          rate = 96.19;
-          break;
-        default:
-          rate = null;
-      }
-      break;
-    case "RUB":
-      switch (targetCurrency) {
-        case "USD":
-          rate = 1 / 87.77;
-          break;
-        case "EUR":
-          rate = 1 / 96.19;
-          break;
-        case "RUB":
-          rate = 1;
-          break;
-        default:
-          rate = null;
-      }
-      break;
+/* 
+Очень интересное решение, спасибо за подсказку, 
+если честно, сам бы не догодался! 
+*/
+const currencyConverter = (amount, from, to) => {
+  switch (`${from}/${to}`) {
+    case "RUB/USD":
+      return amount / 88;
+    case "USD/RUB":
+      return amount * 88;
+    case "RUB/EUR":
+      return amount / 96;
+    case "EUR/RUB":
+      return amount * 96;
+    case "USD/EUR":
+      return amount / 1.1;
+    case "EUR/USD":
+      return amount * 1.1;
+    case "EUR/EUR":
+      return amount;
+    case "USD/USD":
+      return amount;
+    case "RUB/RUB":
+      return amount;
     default:
-      rate = null;
+      return null;
   }
+};
 
-  return rate !== null ? amount * rate : null;
-}
-
-console.log(currencyConverter(1000, "RUB", "RUB"));
+console.log(currencyConverter(1000, "USD", "RUB"));
